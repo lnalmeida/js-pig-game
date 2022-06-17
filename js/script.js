f667ffb4afd3b5c180f89f643dbaa97771fb7b8e
lnalmeida <l.n.almeida.ti@gmail.com>
2022-06-16 23:44:26 -0300

    Final Version

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ let score = [0, 0];
 let currentScore = 0;
 let displayCurrent = HTMLElement;
 let displayScore = HTMLElement;
+let playing = true;
 
 // captura elementos
 let buttonNewGame = document.querySelector('.btn--new');
@@ -19,14 +20,30 @@ let elCurrent_1 = document.querySelector('#current--1');
 let sectionPlayer1 = document.querySelector('section.player--0');
 let sectionPlayer2 = document.querySelector('section.player--1');
 
-// zerando escores
-elScore_0.textContent = 0;
-elScore_1.textContent = 0;
-elCurrent_0.textContent = 0;
-elCurrent_1.textContent = 0;
+const startGame = () => {
+    //zerando as variáveis
+    roundScore = 0;
+    activePlayer = 0;
+    score = [0, 0];
+    currentScore = 0;
+    // zerando escores
+    elScore_0.textContent = 0;
+    elScore_1.textContent = 0;
+    elCurrent_0.textContent = 0;
+    elCurrent_1.textContent = 0;
+    
+    // apagando o dado
+    imgDice.classList.add('hidden');
+    
+    // resetando as classes
+    sectionPlayer1.classList.add('player--active');
+    sectionPlayer2.classList.remove('player--active');
+    sectionPlayer1.classList.remove('player--winner');
+    sectionPlayer2.classList.remove('player--winner');
 
-// apagando o dado
-imgDice.classList.add('hidden');
+    // setando plauing como true
+    playing = true;
+}
 
 
 const setPlayer = (player) => {
@@ -57,27 +74,53 @@ const nextPlayer = () => {
     }
 }
 
+const playerWins = () => {
+        let winner = activePlayer === 0 ? sectionPlayer1 : sectionPlayer2;
+        winner.classList.add('player--winner');
+        winner.classList.remove('player--active');
+        console.log(`Player ${activePlayer} wins!`);
+        imgDice.classList.add('hidden');
+        displayScore.textContent = score[activePlayer];
+        playing = false;
+
+}
+
+
 buttonRoll.addEventListener('click', () => {
-   activePlayer === 0 ? setPlayer(0) : setPlayer(1);
-
-   let dice = Math.trunc(Math.random() * 6) + 1;
-   imgDice.classList.remove('hidden');
-   imgDice.src = `./assets/dice-${dice}.png`;
-
-   if (dice !== 1) {
-       roundScore += dice;
-       displayCurrent.textContent =roundScore;
-   } else {
-       roundScore = 0;
-       displayCurrent.textContent = 0;
-       nextPlayer();
+   if(playing) {
+       activePlayer === 0 ? setPlayer(0) : setPlayer(1);
+   
+      let dice = Math.trunc(Math.random() * 6) + 1;
+      imgDice.classList.remove('hidden');
+      imgDice.src = `./assets/dice-${dice}.png`;
+   
+      if (dice !== 1) {
+          roundScore += dice;
+          displayCurrent.textContent =roundScore;
+      } else {
+          roundScore = 0;
+          displayCurrent.textContent = 0;
+          nextPlayer();
+      }
    }
 });
 
 buttonHold.addEventListener('click', () => {
-    console.log(score)
-    displayScore.textContent = score[activePlayer];
-    score[activePlayer] += roundScore;
-    displayCurrent.textContent = 0;
-    nextPlayer();
+    if(playing) {
+        score[activePlayer] += roundScore;
+        if (!(score[activePlayer] >= 20)) {
+            displayCurrent.textContent = 0;
+            displayScore.textContent = score[activePlayer];
+            console.log(score, score[activePlayer]);
+            nextPlayer();
+        } else {
+            playerWins();
+        }
+    }
 });
+
+buttonNewGame.addEventListener('click', () => {
+    startGame();
+})
+
+startGame();
